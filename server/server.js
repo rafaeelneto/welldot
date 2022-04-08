f65a4256938e50e058daadc55214d03be293ad3e
Rafael Neto <rafaelneto.g@gmail.com>
2022-04-08 12:45:44 -0300

    project restructure

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
-const server = require('./app');
-
-//Initialize the server on the enviroment port
-server.listen(process.env.PORT || 8081, () => {
-  console.log(`Listening on port ${process.env.PORT}`);
-});
